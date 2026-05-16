ae74c3558fce3ebffde55ae8f79e3815da814c3c
mscode07 <msabhithakur7777@gmail.com>
2026-05-16 17:49:08 +0530

    working on api

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use anyhow::Result;
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio :: main]
+async fn main()-> Result<()>{
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")?;
+
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&database_url)
+    .await?;
+
+     sqlx::query("SELECT 1").execute(&pool).await?;
+
+    println!("Database connectec successfulluy!");
+
+    Ok(())
 }
